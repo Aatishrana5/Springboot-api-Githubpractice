87d19a6b961165cea210ec1d8ffccba258700b5e
Aatish Rana <aatishrana315@gmail.com>
2026-03-24 10:24:48 +0530

    First api  commit

---
+++ src/main/java/com/example/Springboot_Github/SpringbootGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Springboot_Github/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.Springboot_Github;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+@RequestMapping("/test")
+public String test() {
+	return "Hello World";
+}
+@RequestMapping("/contact")
+public String contact() {
+	return "Contact Us : 7223999341";
+}
+}
